b15793541b36cb2e92d9ad7dbbc6bea8524d9e34
markdumke <markusdumke@gmail.com>
2017-02-28 11:03:55 +0100

    add n-step TD prediction

--- R/TD.R
+++ R/TD.R
@@ -4,11 +4,12 @@
 #' environment and plugging this into the update equation (Bootstrapping). This
 #'  works also for non-episodic environments.
 #'  
-#'  Currently implemented: TD(0)
+#'  Currently implemented: n-step TD (forward-view)
 #'
 #' @inheritParams evaluatePolicy
 #' @inheritParams predictMC
-#' @param n.steps scalar integer: number of steps
+#' @param n scalar integer: number of steps TD target looks into the 
+#' future
 #'
 #' @return value function
 #' @export
@@ -24,38 +25,111 @@
 #' 
 #' # Estimate state value function with temporal-difference learning
 #' v = TD(random.policy, grid, n.steps = 100, alpha = 0.1)
-TD = function(policy, envir, n.steps = 100, discount.factor = 1, alpha = NULL) {
+TD = function(policy, envir, n.episodes = 1, n = 10, 
+  discount.factor = 1, alpha = 0.1) {
   
-  alpha_input = alpha
-  check_number(discount.factor, lower = 0, upper = 1)
-  if (!is.null(alpha)) {
-    check_number(alpha, lower = 0, upper = 1)
-  }
+  # alpha_input = alpha
+  # check_number(discount.factor, lower = 0, upper = 1)
+  # if (!is.null(alpha)) {
+  #   check_number(alpha, lower = 0, upper = 1)
+  # }
   
   n.states = nrow(policy)
-  n.visits = rep(0, n.states)
+  # n.visits = rep(0, n.states)
   v = rep(0, n.states)
   possible.states = envir$states[envir$states != envir$terminal.states]
   state = sample(possible.states, 1)
   
-  for (i in seq_len(n.steps)) {
-    if (i %% 100 == 0) {
-      print(paste("Step:", i))
+  states = rep(0, n + 1)
+  rewards = rep(0, n + 1)
+  for (i in seq_len(n.episodes)) {
+    
+    # initialize s
+    states[1] = sample(possible.states, 1)
+    
+    time.steps = Inf
+    t = 0
+    
+    while (t < time.steps) {
+      
+      t = t + 1
+      
+      # sample action from policy pi(.|St)
+      action = sample(envir$actions, prob = policy[state, ], size = 1)
+      
+      # observe and store next reward and next state
+      envir$step(states[t], action)
+      states[t + 1] = envir$next.state
+      rewards[t + 1] = envir$reward
+      
+      # if St+1 terminal then time.steps = t + 1
+      if (envir$episode.over == TRUE) {
+        envir$setEpisodeOverFalse()
+        time.steps = t + 1
+      }
+      
+      # which state's value function gets updated
+      tau = t - n + 1
+      
+      
+      if (tau >= 1) {
+        sequ = seq(tau, min(tau + n - 1, time.steps))
+        G = sum(discount.factor ^ sequ * rewards[sequ + 1])
+        if ((tau + n) < time.steps) {
+          G = G + discount.factor^n * v[states[tau + n]]
+        }
+        v[states[tau]] = v[tau] + alpha * (G - v[states[tau]])
+      }
+      
+      if (tau == time.steps - 1) break
     }
-    if (envir$episode.over == TRUE) {
-      envir$setEpisodeOverFalse()
-      state = sample(possible.states, 1)
-    }
-    n.visits[state] = n.visits[state] + 1
-    action = sample(envir$actions, prob = policy[state, ], size = 1)
-    envir$step(state, action)
-    td_error = envir$reward + discount.factor * v[envir$next.state] - v[state]
-    if (is.null(alpha_input)) {
-      alpha = 1 / n.visits[state]
-    }
-    v[state] = v[state] + alpha * td_error
-    state = envir$next.state
+    
   }
   
-  return(v)
+  v
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # n.states = nrow(policy)
+  # n.visits = rep(0, n.states)
+  # v = rep(0, n.states)
+  # possible.states = envir$states[envir$states != envir$terminal.states]
+  # state = sample(possible.states, 1)
+  # 
+  # states = rep(0, iter)
+  # 
+  # for (i in seq_len(iter)) {
+  #   if (i %% 100 == 0) {
+  #     print(paste("Iteration:", i))
+  #   }
+  #   if (envir$episode.over == TRUE) {
+  #     envir$setEpisodeOverFalse()
+  #     state = sample(possible.states, 1)
+  #   }
+  #   n.visits[state] = n.visits[state] + 1
+  #   action = sample(envir$actions, prob = policy[state, ], size = 1)
+  #   envir$step(state, action)
+  #   td_error = envir$reward + discount.factor * v[envir$next.state] - v[state]
+  #   if (is.null(alpha_input)) {
+  #     alpha = 1 / n.visits[state]
+  #   }
+  #   v[state] = v[state] + alpha * td_error
+  #   state = envir$next.state
+  # }
+  # 
+  # return(v)
+}
+
+
+# rewards reward vector
+# v value function from state (S_t+n)
+compute_nstep_return = function(rewards, discount.factor, v) {
+  
+  sum(discount.factor ^ seq(0, length(rewards) - 1) * c(rewards, v))
 }
