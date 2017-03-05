a64fbd224d521cf04c306eaa9c1b8df2d369ca05
markdumke <markusdumke@gmail.com>
2017-03-05 13:06:21 +0100

    add sarsa algorithm

--- R/sarsa.R
+++ R/sarsa.R
@@ -1,12 +1,79 @@
 #' SARSA(lambda)
-#'
+#' 
+#' Currently implemented: Sarsa(0). Sarsa is an on-policy TD control algorithm. 
+#' Action value function Q will be updated 
+#' towards the action value function of the next state and next action using an 
+#' epsilon-greedy policy derived from Q.
+#'   
+#' See Sutton & Bartu(2017) p.138. 
+#'  
 #' @inheritParams evaluatePolicy
+#' @inheritParams predictMC
 #' @param lambda scalar integer between 0 and 1
+#' @param seed scalar integer: random seed
 #'
-#' @return optimal policy and optimal action value function
+#' @importFrom stats runif
+#' @return ptimal action value function Q
 #' @export
-sarsa <- function(policy, envir, lambda, discount.factor) {
+#' @examples 
+#' grid = gridworld$new()
+#' Q = sarsa(grid, n.episodes = 1000)
+sarsa <- function(envir, lambda = NULL, n.episodes = 10, alpha = 0.1, 
+  discount.factor = 1, seed = NULL) {
   
-  print("Currently not implemented.")
+  # input checking
+  if (!is.null(seed)) set.seed(seed)
   
+  # initialize Q randomly
+  n.states = envir$n.states
+  n.actions = envir$n.actions
+  Q = matrix(runif(n.states * n.actions), nrow = n.states, ncol = n.actions)
+  Q[envir$terminal.states, ] = 0
+  
+  for (i in seq_len(n.episodes)) {
+    
+    # initialize starting state s
+    state = sample(envir$non.terminal.states, size = 1)
+    
+    # epsilon-greedy policy, sample action
+    epsilon = 0.1
+    action = sample_epsilon_greedy_action(Q[state, ], epsilon = epsilon)
+    
+    while (state %in% envir$non.terminal.states) {
+      envir$step(state, action)
+      next.state = envir$next.state
+      reward = envir$reward
+      next.action = sample_epsilon_greedy_action(Q[state, ], epsilon = epsilon)
+      
+      # update Q for visited state-action pair using one-step lookahead
+      TD.target = reward + discount.factor * Q[next.state, next.action] 
+      TD.error = TD.target - Q[state, action] 
+      Q[state, action] = Q[state, action] + alpha * TD.error
+      state = next.state
+      action = next.action
+    }
+  }
+  
+  Q
 }
+
+# Q a numeric vector: the action value function for a given state
+# epsilon numeric scalar in [0, 1]: probability of selecting a random action
+# seed integer scalar: seed
+# sample_epsilon_greedy_action(c(1, 2, 3), epsilon = 0.2)
+sample_epsilon_greedy_action <- function(Q, epsilon, seed = NULL) {
+  
+  greedy_action = which.max(Q)
+  random_actions = seq(1, length(Q))
+  # non_greedy_actions = actions[actions != greedy_action]
+  action = sample(c(greedy_action, random_actions), size = 1,  
+    prob = c(1 - epsilon, rep(epsilon, length(random_actions))))
+  
+  action
+}
+
+# # Argmax (ties broken randomly)
+# # x numerical matrix
+# argmax <- function(x) {
+#   max.col(x)
+# }
---
+++ tests/testthat/test_td_learning.R
@@ -0,0 +1,42 @@
+library(reinforcelearn)
+context("Model-free TD Prediction")
+
+set.seed(1477)
+grid = gridworld$new()
+
+# Define random policy
+n.states = nrow(grid$reward.matrix)
+n.actions = ncol(grid$reward.matrix)
+random.policy = matrix(1 / n.actions, nrow = n.states, ncol = n.actions)
+
+# Compare results with expected result
+v.expected = c(0, -14, -20, -22, -14, -18, -20, -20,
+  -20, -20, -18, -14, -22, -20, -14, 0)
+
+# test_that("TD Prediction works for gridworld", {
+#   v = TD(random.policy, grid, n = 2, n.episodes = 10000, alpha = 0.01)
+#   expect_equal(v, v.expected, tolerance = 0.1)
+#   # what if n > length(episode)?
+#   # v = TD(random.policy, grid, n = 100, n.episodes = 10000, alpha = 0.01)
+#   # expect_equal(v, v.expected, tolerance = 0.1)
+# })
+
+grid$setEpisodeOverFalse()
+left = c(1, 0, 0, 0)
+right = c(0, 1, 0, 0)
+up = c(0, 0, 1, 0)
+down = c(0, 0, 0, 1)
+# there are more than one optimal policy because of symmetry
+optimal.policy = matrix(c(left, left, left, left, 
+  up, up, right, down, 
+  up, up, right, down, 
+  up, right, right, left), ncol = 4, byrow = TRUE)
+# test only states for which optimal policy is distinct
+test_states = c(2, 3, 5, 8, 9, 12, 14, 15)
+
+test_that("SARSA(0) gives correct policy for gridworld", {
+  Q = sarsa(grid, n.episodes = 1000)
+  policy = make_greedy_policy(Q)
+  # expect_equal(Q, Q.expected)
+  expect_equal(policy[test_states, ], optimal.policy[test_states, ])
+})
