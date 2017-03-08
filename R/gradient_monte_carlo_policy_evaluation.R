c96c6f6905b2491748a6634838d38873911e48eb
markdumke <markusdumke@gmail.com>
2017-03-08 12:07:17 +0100

    add gradientMCPolicyEvaluation.R

---
+++ R/gradient_monte_carlo_policy_evaluation.R
@@ -0,0 +1,16 @@
+#' Gradient Monte Carlo Policy Evaluation using Function Approximation
+#'
+#' Evaluate a given policy using.
+#' @param envir 
+#' @param policy 
+#' @param value.function 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gradientMCPolicyEvaluation <- function(envir, policy, value.function) {
+  
+  
+  
+}
