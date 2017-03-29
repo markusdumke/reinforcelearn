7dde0df3c7ea0012613b18ecd67f7807ad73c0ac
markdumke <markusdumke@gmail.com>
2017-03-29 16:41:42 +0200

    update windy gridworld vignette

--- vignettes/solve-the-windy-gridworld-using-sarsa.R
+++ vignettes/solve-the-windy-gridworld-using-sarsa.R
@@ -4,23 +4,24 @@ grid = WindyGridworld$new()
 WindyGridworld1 = makeEnvironment(transition.array = grid$transition.array, 
   reward.matrix = grid$reward.matrix, 
   terminal.states = grid$terminal.states, 
-  initial.state = grid$initial.state)
+  initial.state = 30)
 
 ## ---- results = "hide"---------------------------------------------------
-res = sarsa(WindyGridworld1, lambda = 0, learning.rate = 0.5, epsilon = 0.1, n.steps = 8000)
-# res = qlearning(WindyGridworld1, n.episodes = 100000)
-Q = res$Q
+res = sarsa(WindyGridworld1, n.episodes = 1000, seed = 123)
 
 ## ------------------------------------------------------------------------
-print(matrix(apply(res$Q, 1, max), ncol = 10, byrow = TRUE))
-# Greedy policy with respect to Q
-optimal.policy = max.col(Q)
+# Optimal action value function
+optimal.Q = apply(res$Q, 1, max)
+print(matrix(optimal.Q, ncol = 10, byrow = TRUE))
+
+# Optimal policy
+optimal.policy = max.col(res$Q)
 print(matrix(optimal.policy, ncol = 10, byrow = TRUE))
 
 ## ------------------------------------------------------------------------
-plot(x = res$time.steps.episode, y = seq_along(res$time.steps.episode),
- type = "l", xlim = c(0, 8000), ylab = "Episode", xlab = "Time steps",
+plot(x = cumsum(res$steps.per.episode)[1:300], y = 1:300,
+ type = "l", xlim = c(0, 20000), ylab = "Number of Episodes", xlab = "Time steps",
  main = "Episodes completed per time step")
-plot(x = seq_along(res$time.steps.episode), y = diff(c(0, res$time.steps.episode)),
- type = "l", xlim = c(0, length(res$time.steps.episode)), ylab = "Episode length", xlab = "Episode", main = "Episode length over time")
+plot(x = seq_along(res$steps.per.episode), y = res$steps.per.episode,
+ type = "l", xlim = c(0, 1000), ylab = "Episode length", xlab = "Episode", main = "Episode length over time")
 
