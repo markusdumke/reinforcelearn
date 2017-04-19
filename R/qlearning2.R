8294e63667d20cec00c854f3c1188cb835212cd3
markdumke <markusdumke@gmail.com>
2017-04-19 17:00:59 +0200

    update qlearning2

--- R/qlearning2.R
+++ R/qlearning2.R
@@ -1,8 +1,8 @@
 #' Q-Learning with Function Approximation
 #'
-#' Q-Learning algorithm with Experience Replay and 
+#' Q-Learning algorithm with Experience Replay and
 #' Frozen Target Network and Double Q-Learning.
-#' 
+#'
 #' To use experience replay you can
 #' either specify an initial replay memory filled with experience
 #' and provide the size of the replay memory. If you do not
@@ -15,13 +15,13 @@
 #' @return list with weights and number of steps
 #' @export
 #' @seealso [qlearning]
-#' @examples 
+#' @examples
 #' # define the environment
 #' grid = WindyGridworld$new()
 #' WindyGridworld1 = makeEnvironment(transition.array = grid$transition.array,
 #'   reward.matrix = grid$reward.matrix,
 #'   initial.state = 30L)
-#' 
+#'
 #' # Build the DQN
 #' # define a tensorflow graph for the function approximator (here a neural network)
 #' library(tensorflow)
@@ -40,11 +40,11 @@
 #' optimizer = tf$train$GradientDescentOptimizer(learning_rate = 0.1)
 #' trainModel = optimizer$minimize(loss)
 #' copy.params = tf$assign(weights2, weights)
-#' 
+#'
 #' # initialize the session and the weights
 #' sess = tf$Session()
 #' sess$run(tf$global_variables_initializer())
-#' 
+#'
 #' # takes the state and returns a one-hot vector
 #' preprocessState = function(state_) {
 #'   one_hot = matrix(0L, nrow = length(state_), ncol = WindyGridworld1$n.states)
@@ -63,48 +63,48 @@
 #'   sess$run(tuple(trainModel, weights),
 #'     feed_dict = dict(inputs = inputs_, nextQ = outputs_))
 #' }
-#' 
+#'
 #' # copy model weights to target
 #' copy = function() {
 #'   sess$run(copy.params)
 #' }
-#' 
+#'
 #' # DQN
 #' res = qlearning2(WindyGridworld1, n.episodes = 300L,
 #'   preprocessState, predict, train, seed = 2L)
-#' 
+#'
 #' # please rerun the tensorflow model building
 #' # DQN with experience replay
 #' res2 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #'   preprocessState, predict, train,
 #'   experience.replay = TRUE, replay.memory.size = 10000L,
 #'   initial.replay.memory.size = 10000L, seed = 2L)
-#' 
+#'
 #' # DQN with frozen target network
 #' res3 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #'   preprocessState, predict, train, predict2, copy,
 #'   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
-#' 
+#'
 #' # DQN with experience replay and frozen target network
 #' res4 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #'   preprocessState, predict, train, predict2, copy,
 #'   experience.replay = TRUE, replay.memory.size = 10000L,
 #'   initial.replay.memory.size = 10000L,
 #'   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
-#'   
+#'
 #' # DQN with frozen target network and double q-learning
 #' res = qlearning2(WindyGridworld1, n.episodes = 300L,
-#'   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE, 
+#'   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE,
 #'   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
 #' # double qlearning currently only works for frozen targets
-#' 
+#'
 #' # DQN with experience replay, frozen target network and double q-learning
 #' res = qlearning2(WindyGridworld1, n.episodes = 300L,
-#'   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE, 
-#'   experience.replay = TRUE, replay.memory.size = 10000L, 
+#'   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE,
+#'   experience.replay = TRUE, replay.memory.size = 10000L,
 #'   initial.replay.memory.size = 10000L,
 #'   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
-#' 
+#'
 #' # DQN with experience replay passed on to function
 #' # pass initial random experience to the function
 #' replay.memory.size = 10000L # steps
@@ -122,26 +122,31 @@
 #'     WindyGridworld1$reset()
 #'   }
 #' }
-#' 
+#'
 #' res5 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #'   preprocessState, predict, train,
 #'   experience.replay = TRUE, replay.memory = replay.memory2,
 #'   replay.memory.size = 12000L, seed = 2L)
-#'   
-#' 
-qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL, 
+#'
+#'
+qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
   predict = NULL, train = NULL, predict2 = NULL, copy = NULL,
   double.qlearning = FALSE, experience.replay = FALSE, replay.memory = NULL,
-  replay.memory.size = 1000L, initial.replay.memory.size = 1000L, 
+  replay.memory.size = 1000L, initial.replay.memory.size = 1000L,
   batch.size = 32L, frozen.target = FALSE, update.target.after = 100L,
   epsilon = 0.1, epsilon.decay = 0.5, epsilon.decay.after = 100L,
   discount.factor = 1, seed = NULL, ...) {
-
+  
   if (!is.null(seed)) { set.seed(seed) } # set random seed
+  if (double.qlearning == TRUE) {
+    if(frozen.target == FALSE) {
+      stop("Double Q-Learning only works when frozen.target == TRUE")
+    }
+  }
   # if (replay.memory.size < length(replay.memory)) {
   #   stop("replay.memory.size must be at least as large as the length of provided replay.memory.")
   # }
-
+  
   # experience replay
   # fill initial replay memory randomly if no experience is supplied
   if (experience.replay == TRUE) {
@@ -171,28 +176,28 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
     initial.replay.memory.size = 1L
     batch.size = 1L
   }
-
+  
   # statistics about learning behaviour: steps per episode
   steps.per.episode = rep(0L, n.episodes)
   steps = 0L # counts total number of steps
   replay.steps = 0L
-
+  
   for (i in seq_len(n.episodes)) {
     envir$reset()
     state = envir$state # initial state
     steps.this.episode = 0L # counts number of steps in current episode
-
+    
     while (envir$episode.over == FALSE) {
       steps.this.episode = steps.this.episode + 1L # increase step counter
       steps = steps + 1L
       replay.steps = replay.steps + 1L
-
+      
       features.state_ = preprocessState(state, ...)
       Q.state = predict(features.state_, ...)
       action = sample_epsilon_greedy_action(Q.state, epsilon)
       envir$step(action)
       next.state = envir$state
-
+      
       # if replay memory is full, replace oldest experience
       if (experience.replay == TRUE) {
         if (initial.replay.memory.size + steps <= replay.memory.size) {
@@ -207,35 +212,41 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
         replay.memory[[1L]] = list(action = action, reward = envir$reward,
           state = state, next.state = next.state)
       }
-
+      
       # sample a batch of transitions s, a, r, s' from replay.memory
       indexes = sample(seq_len(min(replay.memory.size,
         initial.replay.memory.size + steps)), batch.size)
       batch = replay.memory[indexes]
       batch.states = lapply(batch, "[[", "state")
       batch.next.states = lapply(batch, "[[", "next.state")
-      batch.actions = vapply(batch, "[[", "action", FUN.VALUE = double(1)) # sapply(batch, "[[", "action")
-      batch.rewards = vapply(batch, "[[", "reward", FUN.VALUE = double(1)) # sapply(batch, "[[", "reward")
-      features.state = Reduce(rbind, lapply(batch.states, preprocessState))
-      features.next.state = Reduce(rbind, lapply(batch.next.states, preprocessState))
-
-      # frozen target, update weights only occasionaly
-      if (frozen.target == TRUE) {
-        Q.state = predict2(features.state, ...)
-        Q.next.state = predict2(features.next.state, ...) # use frozen target network
+      batch.actions = vapply(batch, "[[", "action", FUN.VALUE = double(1))
+      batch.rewards = vapply(batch, "[[", "reward", FUN.VALUE = double(1))
+      features.batch.states = Reduce(rbind, lapply(batch.states, preprocessState))
+      features.batch.next.states = Reduce(rbind, lapply(batch.next.states, preprocessState))
+      
+      # our current estimate of the q values
+      Q.state = predict(features.batch.states, ...)
+      
+      # compute target values
+      # for double qlearning action selection and evaluation are decoupled
+      if (frozen.target == FALSE) {
+        Q.next.state = predict(features.batch.next.states, ...)
+        td.target =  batch.rewards + discount.factor * apply(Q.next.state, 1, max)
+        # td.target =  batch.rewards + discount.factor * Q.next.state[argmax(Q.next.state)] # equivalent to the above
       } else {
-        Q.state = predict(features.state, ...)
-        Q.next.state = predict(features.next.state, ...)
+        if (double.qlearning == FALSE) {
+          Q.next.state = predict2(features.batch.next.states, ...)
+          td.target =  batch.rewards + discount.factor * apply(Q.next.state, 1, max)
+        } else {
+          Q.next.state = predict2(features.batch.next.states, ...)
+          Q.next.state.online = predict(features.batch.next.states, ...)
+          td.target =  batch.rewards + discount.factor * Q.next.state[argmax(Q.next.state.online)]
+        }
       }
-      if (double.qlearning == TRUE) {
-         Q.next.state.online = predict(features.next.state, ...)
-         td.target =  batch.rewards + discount.factor * Q.next.state[argmax(Q.next.state.online)]
-      }
-      td.target =  batch.rewards + discount.factor * apply(Q.next.state, 1, max) # max over rows
+      
       target.Q = Q.state
       target.Q[cbind(seq_along(batch.actions), batch.actions + 1)] = td.target
-
-      weights = train(features.state, target.Q, Q.state, ...) # train on minibatch
+      weights = train(features.batch.states, target.Q, Q.state, ...) # train on minibatch
       
       # update target network: get all weights and copy them to target network
       if (frozen.target == TRUE) {
@@ -245,9 +256,9 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
           # print("Copied model parameters to target network.")
         }
       }
-
+      
       state = next.state
-
+      
       if (envir$episode.over) {
         if (i %% epsilon.decay.after == 0L) {
           epsilon = epsilon * epsilon.decay
@@ -269,28 +280,28 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
 # # no experience replay, no frozen targets
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res = qlearning2(WindyGridworld1, n.episodes = 300L,
-#   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE, 
+#   preprocessState, predict, train, predict2, copy, double.qlearning = TRUE,
 #   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
 # # double qlearning only works for frozen targets
-# 
+#
 # # no experience replay, no frozen targets
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res = qlearning2(WindyGridworld1, n.episodes = 300L,
 #   preprocessState, predict, train, seed = 2L)
-# 
+#
 # # with experience replay
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res2 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #   preprocessState, predict, train,
 #   experience.replay = TRUE, replay.memory.size = 10000L,
 #   initial.replay.memory.size = 10000L, seed = 2L)
-# 
+#
 # # no experience replay, with frozen targets
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res3 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #   preprocessState, predict, train, predict2, copy,
 #   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
-# 
+#
 # # with experience replay, with frozen targets
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res4 = qlearning2(WindyGridworld1, n.episodes = 300L,
@@ -298,7 +309,7 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
 #   experience.replay = TRUE, replay.memory.size = 10000L,
 #   initial.replay.memory.size = 10000L,
 #   frozen.target = TRUE, update.target.after = 100L, seed = 2L)
-# 
+#
 # # with experience replay passed on to function
 # # pass initial random experience to the function
 # replay.memory.size = 10000L # steps
@@ -316,7 +327,7 @@ qlearning2 <- function(envir, n.episodes = 10L, preprocessState = NULL,
 #     WindyGridworld1$reset()
 #   }
 # }
-# 
+#
 # source("C:/Users/M/Desktop/reinforcelearn/R/build_model.R")
 # res5 = qlearning2(WindyGridworld1, n.episodes = 300L,
 #   preprocessState, predict, train,
