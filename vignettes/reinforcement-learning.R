7416590d2094090b38dfe753f47adcd9debceb4a
markdumke <markusdumke@gmail.com>
2017-06-26 13:23:58 +0200

    update vignette

--- vignettes/reinforcement-learning.R
+++ vignettes/reinforcement-learning.R
@@ -1,3 +1,7 @@
+## ---- eval = FALSE-------------------------------------------------------
+#  install.packages("devtools")
+#  devtools::install_github("markdumke/reinforcelearn")
+
 ## ------------------------------------------------------------------------
 library(reinforcelearn)
 
@@ -10,31 +14,20 @@ Env = makeEnvironment(transition.array = t, reward.matrix = r)
 #  MCar = makeEnvironment("MountainCar-v0")
 
 ## ---- eval = FALSE-------------------------------------------------------
+#  MCar$action.space # Discrete
+#  MCar$state.space.bounds # [-1.2, 0.6], [-0.07, 0.07]
+
+## ---- eval = FALSE-------------------------------------------------------
+#  MCar$reset()
+#  for (i in 1:200) {
+#    action = sample(MCar$actions, 1)
+#    MCar$step(action)
+#  }
+#  MCar$close()
+
+## ---- eval = FALSE-------------------------------------------------------
 #  res = sarsa(Env)
 
-## ------------------------------------------------------------------------
-library(reinforcelearn)
-grid = WindyGridworld$new()
-WindyGridworld1 = makeEnvironment(transition.array = grid$transition.array, 
-  reward.matrix = grid$reward.matrix, 
-  initial.state = 30L)
-
-## ---- results = "hide"---------------------------------------------------
-res = sarsa(WindyGridworld1, n.episodes = 1000, seed = 123)
-
-## ------------------------------------------------------------------------
-# Optimal action value function
-optimal.Q = apply(res$Q, 1, max)
-print(matrix(optimal.Q, ncol = 10, byrow = TRUE))
-
-# Optimal policy
-optimal.policy = max.col(res$Q)
-print(matrix(optimal.policy, ncol = 10, byrow = TRUE))
-
-## ------------------------------------------------------------------------
-plot(x = cumsum(res$steps.per.episode)[1:300], y = 1:300,
- type = "l", xlim = c(0, 20000), ylab = "Number of Episodes", xlab = "Time steps",
- main = "Episodes completed per time step")
-plot(x = seq_along(res$steps.per.episode), y = res$steps.per.episode,
- type = "l", xlim = c(0, 1000), ylab = "Episode length", xlab = "Episode", main = "Episode length over time")
+## ---- eval = FALSE-------------------------------------------------------
+#  
 
