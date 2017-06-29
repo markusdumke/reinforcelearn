5e8cc88251e493080a66cf96a79094282aa972e0
markdumke <markusdumke@gmail.com>
2017-06-29 12:15:10 +0200

    move call to python script into startServer function

--- R/startServer.R
+++ R/startServer.R
@@ -1,6 +1,10 @@
-.onLoad <- function(libname, pkgname) {
+#' Start Python Server
+#'
+#' @export
+#'
+startServer = function() {
   package.path = system.file(package = "reinforcelearn")
   path2pythonfile = paste0(package.path, "/gym_http_server.py")
-  command <- "python"
+  command = "python"
   system2(command, args = path2pythonfile, stdout = NULL, wait = FALSE)
-} # what if python is not installed?? -> error
+}
