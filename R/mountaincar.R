fa7777b226001e317930211365bc1f7a3b275402
markdumke <markusdumke@gmail.com>
2017-09-29 10:56:52 +0200

    add mountain car environment

---
+++ R/mountaincar.R
@@ -0,0 +1,70 @@
+#' Mountain Car
+#' 
+#' @references Sutton and Barto (1998)
+#' @export
+#' @examples 
+#' m = MountainCar()
+#' m$reset()
+#' m$step(1)
+#' 
+MountainCar = function() {
+  MountainCarClass$new()
+}
+
+MountainCarClass = R6::R6Class("MountainCar",
+  public = list(
+    action.space = "Discrete",
+    actions = 0:2,
+    n.actions = 3,
+    state.space = "Box",
+    state.space.bounds = list(c(-1.2, 0.5), c(-0.07, 0.07)),
+    done = FALSE,
+    n.steps = 0,
+    state = NULL,
+    previous.state = NULL,
+    reward = NULL,
+    velocity = NULL,
+    position = NULL,
+
+    reset = function() {
+      self$n.steps = 0
+      self$previous.state = NULL
+      self$done = FALSE
+      self$position = runif(1, - 0.6, - 0.4)
+      self$velocity = 0
+      self$state = matrix(c(self$position, self$velocity), ncol = 2)
+      invisible(self)
+    },
+
+    step = function(action) {
+      self$previous.state = self$state
+      self$n.steps = self$n.steps + 1
+
+      self$velocity = self$velocity + 0.001 * (action - 1) -
+        0.0025 * cos(3 * self$position)
+      if (self$velocity < self$state.space.bounds[[2]][1]) {
+        self$velocity = self$state.space.bounds[[2]][1]
+      }
+      if (self$velocity > self$state.space.bounds[[2]][2]) {
+        self$velocity = self$state.space.bounds[[2]][2]
+      }
+      self$position = self$position + self$velocity
+      if (self$position < self$state.space.bounds[[1]][1]) {
+        self$position = self$state.space.bounds[[1]][1]
+        self$velocity = 0
+      }
+
+      self$state = matrix(c(self$position, self$velocity), ncol = 2)
+      self$reward = - 1
+      if (self$position >= 0.5) {
+        self$done = TRUE
+        self$reward = 0
+      }
+      invisible(self)
+    },
+
+    close = function() {
+      invisible(self)
+    }
+  )
+)
