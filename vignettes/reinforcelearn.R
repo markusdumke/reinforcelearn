c90124decc7723f2b9fbbb952d047e84ce9721b0
Dumke <Markus.Dumke@campus.lmu.de>
2017-10-11 18:27:22 +0200

    update intro vignette

--- vignettes/reinforcelearn.R
+++ vignettes/reinforcelearn.R
@@ -1,36 +1,27 @@
-## ---- eval = FALSE-------------------------------------------------------
-#  install.packages("devtools")
-#  devtools::install_github("markdumke/reinforcelearn")
-
 ## ------------------------------------------------------------------------
 library(reinforcelearn)
 
 ## ------------------------------------------------------------------------
-library(reinforcelearn)
-
-P = array(0, c(2,2,2))
-P[, , 1] = matrix(c(0.5, 0.5, 0.8, 0.2), 2, 2, byrow = TRUE)
-P[, , 2] = matrix(c(0, 1, 0.1, 0.9), 2, 2, byrow = TRUE)
+P = array(0, c(2, 2, 2))
+P[, , 1] = matrix(c(0.5, 0.5, 0, 1), 2, 2, byrow = TRUE)
+P[, , 2] = matrix(c(0, 1, 0, 1), 2, 2, byrow = TRUE)
 R = matrix(c(5, 10, -1, 2), 2, 2, byrow = TRUE)  
 env = makeEnvironment(transitions = P, rewards = R)
 
-## ---- eval = FALSE-------------------------------------------------------
-#  MountainCar = makeEnvironment("MountainCar-v0")
+## ------------------------------------------------------------------------
+paste("State space:" , env$state.space)
+paste("Number of actions:", env$n.actions)
 
-## ---- eval = FALSE-------------------------------------------------------
-#  MountainCar$action.space # Discrete
-#  MountainCar$state.space.bounds # [-1.2, 0.6], [-0.07, 0.07]
-
-## ---- eval = FALSE-------------------------------------------------------
-#  MountainCar$reset()
-#  for (i in 1:200) {
-#    action = sample(MountainCar$actions, 1)
-#    MountainCar$step(action)
-#  }
-#  MountainCar$close()
+print("Reset the environment")
+env$reset()
+paste("The state is initially", env$state)
+print("Take action 1")
+env$step(1)
+paste("The state is now", env$state)
+paste("And the reward for action 1 was", env$reward)
 
 ## ------------------------------------------------------------------------
-# Solve the windy gridworld task using Sarsa.
-env = windyGridworld()
-res = sarsa(env)
+# Solve the windy gridworld task using Q-Learning
+# env = windyGridworld()
+# res = qlearning(env)
 
