7520db3fb301f5aec9ceb679d5e2841dc243061f
markdumke <markusdumke@gmail.com>
2017-10-25 19:58:17 +0200

    add some tests

---
+++ tests/testthat/test_helper_functions.R
@@ -0,0 +1,19 @@
+library(reinforcelearn)
+context("actorCritic")
+
+test_that("softmax implementation is correct", {
+  x = matrix(1:3, ncol = 3)
+  numerator = sum(exp(x))
+  expect_equal(softmax(x), matrix(c(exp(1) / numerator, exp(2) / numerator, exp(3) / numerator), ncol = 3))
+})
+
+test_that("identity2 returns its first argument", {
+  expect_equal(identity2(2, 1), 2)
+  expect_equal(identity2(7, ""), identity(7))
+})
+
+
+test_that("makeNHot returns n hot vector", {
+  expect_equal(makeNHot(2, 4, out = "matrix"), matrix(c(0, 1, 0, 0), ncol = 4))
+  expect_equal(makeNHot(1, 2, out = "vector"), c(1, 0))
+})
