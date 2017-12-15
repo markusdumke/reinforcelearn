d96b926ed1b726d35f37f49f0489159e555fabc1
Markus Dumke <markusdumke@gmail.com>
2017-12-15 14:24:17 +0100

    add policy class

---
+++ R/policy.R
@@ -0,0 +1,147 @@
+#' Create policy.
+#'
+#' Reinforcement learning policies.
+#'
+#' @param class \[`character(1)`] \cr
+#'   Class of policy. See Section Policies for available policies.
+#' @param ... \[`any`] \cr
+#'   Arguments passed on to the policy class.
+#'
+#' @md
+#' @aliases Policy
+#'
+#' @section Policies:
+#' * [RandomPolicy]
+#' * [GreedyPolicy]
+#' * [EpsilonGreedyPolicy]
+#' * [SoftmaxPolicy]
+#'
+#' @export
+#' @examples
+#' policy = makePolicy("random")
+#' policy = makePolicy("epsilon.greedy", epsilon = 0.1)
+makePolicy = function(class = "random", ...) {
+  checkmate::assertChoice(class,
+    c("random", "epsilon.greedy", "greedy", "softmax")) #, "gaussian"))
+  switch(class,
+    random = RandomPolicy$new(...), # default
+    epsilon.greedy = EpsilonGreedyPolicy$new(...),
+    greedy = GreedyPolicy$new(...),
+    softmax = SoftmaxPolicy$new(...)
+  )
+}
+
+
+Policy = R6::R6Class("Policy",
+  public = list(
+    sampleAction = function(policy) {
+      action = sample(seq_along(policy), prob = policy,
+        size = 1, replace = TRUE) - 1L
+      action
+    }
+  )
+)
+
+#' Epsilon Greedy Policy
+#'
+#' @aliases GreedyPolicy
+#' @export
+#' @section Usage:
+#' \code{makePolicy("epsilon.greedy", epsilon)} \cr
+#' \code{makePolicy("greedy")}
+#'
+#' @param epsilon [\code{numeric(1) in [0, 1]}] \cr
+#'   Ratio of random exploration in epsilon-greedy action selection.
+#'
+#' @importFrom nnet which.is.max
+#' @name EpsilonGreedyPolicy
+#' @examples
+#' policy = makePolicy("epsilon.greedy", epsilon = 0.1)
+#' (probs = policy$getActionProbs(matrix(c(1:3), ncol = 3), n.actions = 3))
+#' policy$sampleAction(probs)
+NULL
+
+EpsilonGreedyPolicy = R6::R6Class("EpsilonGreedyPolicy",
+  inherit = Policy,
+  public = list(
+    epsilon = NULL,
+    getActionProbs = function(Q, n.actions) { # fixme: break ties
+      greedy.action = nnet::which.is.max(Q)
+      policy = matrix(0, nrow = 1, ncol = n.actions)
+      policy[, greedy.action] = 1 - self$epsilon
+      policy = policy + self$epsilon / n.actions
+      policy
+    },
+    initialize = function(epsilon) {
+      self$epsilon = epsilon
+    }
+  )
+)
+
+GreedyPolicy = R6::R6Class("GreedyPolicy",
+  # inherit = EpsilonGreedyPolicy,
+  public = list(
+    getActionProbs = function(Q, n.actions) {
+      greedy.action = nnet::which.is.max(Q) # this is duplicate code!
+      policy = matrix(0, nrow = 1, ncol = n.actions)
+      policy[, greedy.action] = 1
+      policy
+    }
+  )
+)
+
+#' Random Policy
+#'
+#' @export
+#' @section Usage:
+#' \code{makePolicy("random")}
+#'
+#' @name RandomPolicy
+#' @examples
+#' pol = makePolicy("random")
+#' (probs = pol$getActionProbs(n.actions = 4))
+#' (probs = pol$getActionProbs(n.actions = 5))
+#' pol$sampleAction(probs)
+NULL
+
+RandomPolicy = R6::R6Class("RandomPolicy",
+  inherit = Policy,
+  public = list(
+    getActionProbs = function(Q, n.actions) {
+      policy = matrix(1 / n.actions, nrow = 1, ncol = n.actions)
+      policy
+    }
+  )
+)
+
+GaussianPolicy = R6::R6Class("GaussianPolicy",
+  inherit = Policy,
+  public = list(
+    sampleAction = function(mean, sd) {
+      rnorm(1L, mean, sd)
+    }
+  )
+)
+
+#' Softmax Policy
+#'
+#' @export
+#' @section Usage:
+#' \code{makePolicy("softmax")}
+#'
+#' @name SoftmaxPolicy
+#' @examples
+#' pol = makePolicy("softmax")
+#' (probs = pol$getActionProbs(matrix(c(1:3), ncol = 3)))
+#' pol$sampleAction(probs)
+NULL
+
+SoftmaxPolicy = R6::R6Class("SoftmaxPolicy",
+  inherit = Policy,
+  public = list(
+    getActionProbs = function(Q, n.actions) {
+      policy = exp(Q) / rowSums(exp(Q))
+      policy
+    }
+  )
+)
